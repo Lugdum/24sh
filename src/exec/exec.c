fbd8072fc6017fa26fcf458499e171641d7ea281
Victor Buthod <victor.buthod@epita.fr>
2023-01-11 16:21:54 +0100

    main

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -2,73 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "exec.h"
 #include "../parser/parser.h"
-
-int exec_command(struct Node *ast) {
-  if (!strcmp(ast->children[0]->value, "echo")) {
-    for (int i = 1; i < ast->nb_children; i++) {
-      printf("%s ", ast->children[1]->value); // printf pour l'instant, je pense
-                                              // pas que ce soit autorisé
-    }
-    printf("\n");
-    return 1; // return 1 on success of echo, else 0
-  }
-  if (!strcmp(ast->children[0]->value, "true")) {
-    return 1;
-  }
-  if (!strcmp(ast->children[0]->value, "false")) {
-    return 0;
-  }
-  return 1;
-}
-
-int process_if(struct Node *ast) {
-  if (node_type(ast->children[0]))
-    return node_type(ast->children[1]);
-  else
-    return node_type(ast->children[2]);
-}
-
-int process_cmd(struct Node *ast) {
-  return node_type(ast->children[0]);
-}
-
-int process_list(struct Node *ast) {
-  int r = 0;
-  for (int i = 0; i < ast->nb_children; i++)
-    r = node_type(ast->children[i]);
-  return r;
-}
-
-int process_pipe(struct Node *ast) {
-  //int fd = -1;
-  node_type(ast->children[0]);
-  return node_type(ast->children[1]);
-}
+#include "exec.h"
 
 int echo(struct Node *ast, int fd) {
   int flag_n = 0;
   int flag_e = 0;
   int flag_E = 0;
   int word = 0;
-  int son = 1;
+  int son = 0;
   while (!word && son < ast->nb_children) {
     son++;
     char *flag = ast->children[son]->value;
-    if (strcmp(flag, "-e"))
+    if (!strcmp(flag, "-e")) {
       flag_e = 1;
-    else if (strcmp(flag, "-E"))
+    } else if (!strcmp(flag, "-E")) {
       flag_E = 1;
-    else if (strcmp(flag, "-n"))
+    } else if (!strcmp(flag, "-n")) {
       flag_n = 1;
-    else if (strcmp(flag, "-en")) {
+    } else if (!strcmp(flag, "-en")) {
       flag_e = 1;
       flag_n = 1;
-    } else if (strcmp(flag, "-En")) {
+    } else if (!strcmp(flag, "-ne")) {
+      flag_e = 1;
+      flag_n = 1;
+    } else if (!strcmp(flag, "-En")) {
       flag_E = 1;
       flag_n = 1;
-    } else if (strcmp(flag, "-Ee")) {
+    } else if (!strcmp(flag, "-Ee")) {
       flag_e = 1;
       flag_E = 1;
     } else
@@ -116,7 +77,7 @@ int echo(struct Node *ast, int fd) {
     }
 
     // Ajoute un espace entre chaque argument, sauf pour le dernier
-    if (i < ast->nb_children - son - 1) {
+    if (i < ast->nb_children - son) {
       putchar(' ');
     }
   }
@@ -126,28 +87,67 @@ int echo(struct Node *ast, int fd) {
   }
   return 1;
 }
+int exec_command(struct Node *ast) {
+  if (!strcmp(ast->children[0]->value, "echo")) {
+    return echo(ast, 1);
+  }
+  if (!strcmp(ast->children[0]->value, "true")) {
+    return 1;
+  }
+  if (!strcmp(ast->children[0]->value, "false")) {
+    return 0;
+  }
+  return 1;
+}
+
+int process_if(struct Node *ast) {
+  if (node_type(ast->children[0]))
+    return node_type(ast->children[1]);
+  else
+    return node_type(ast->children[2]);
+}
+
+int process_cmd(struct Node *ast) { 
+    if (ast->children[0]->type == AST_SIMPLE_COMMAND)
+        return node_type(ast->children[0]);
+    else
+        return exec_command(ast); 
+}
+
+int process_list(struct Node *ast) {
+  int r = 0;
+  for (int i = 0; i < ast->nb_children; i++)
+    r = node_type(ast->children[i]);
+  return r;
+}
+
+int process_pipe(struct Node *ast) {
+  // int fd = -1;
+  node_type(ast->children[0]);
+  return node_type(ast->children[1]);
+}
 
 int node_type(struct Node *ast) {
   switch (ast->type) {
   case AST_INPUT:
-    return process_cmd(ast);  
+    return process_cmd(ast);
   case AST_LIST:
     return process_list(ast);
   case AST_AND_OR:
-//TODO
+    // TODO
     break;
   case AST_PIPELINE:
-//TODO
+    // TODO
     break;
   case AST_COMMAND:
-    return process_cmd(ast);  
+    return process_cmd(ast);
   case AST_SIMPLE_COMMAND:
     return exec_command(ast);
   case AST_ELEMENT:
-//TODO
+    // TODO
     break;
   case AST_CMD:
-//TODO
+    // TODO
     break;
   case AST_IF:
     return process_if(ast);
@@ -155,5 +155,5 @@ int node_type(struct Node *ast) {
     printf("Unknown Node type\n");
     return 2;
   }
-    return 0;
+  return 0;
 }
