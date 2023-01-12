670f72a683d124b6782062364231fb223c097600
Roibooh <guillaume.mercier@epita.fr>
2023-01-12 23:44:17 +0100

    Update exec.h

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -5,7 +5,10 @@
 #include "../parser/parser.h"
 #include "../variable/variable.h"
 
+#ifndef VARLIST
+#define VARLIST
 struct variable_list *var_list;
+#endif
 
 int main_exec(struct Node *ast);
 int node_type(struct Node *ast);
